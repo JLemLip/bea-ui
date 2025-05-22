48140cfc23764314b555dd8d5dd0b0da478a5f43
krbx <kerby.marcelino@gmail.com>
2025-05-22 16:32:44 +0800

    Normalize line endings

--- src/app/(app)/dashboard/page.js
+++ src/app/(app)/dashboard/page.js
@@ -18,6 +18,14 @@ const Dashboard = () => {
                             show department score, if super admin show overall
                         </div>
                     </div>
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 bg-white border-b border-gray-200">
+                            Year Selection and Chart Dashboard Header IF BM then
+                            show branch ratings for current trime, with option
+                            to select different year and trime. if exec then
+                            show department score, if super admin show overall
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
