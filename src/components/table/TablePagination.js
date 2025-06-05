a3c780925c39a0651b67905fa97a508fe9db4d21
Christian John Borjal <128572406+BorjalCj@users.noreply.github.com>
2025-06-05 20:52:43 +0800

    for pagination

--- src/components/table/TablePagination.js
+++ src/components/table/TablePagination.js
@@ -1,90 +1,67 @@
-const TablePagination = () => {
+// const PagitionRow = rows => {
+//     const map_rows = []
+//     rows.forEach((row, index) => {
+
+//     })
+// }
+
+const PaginationLogo = ({ isNext }) => {
+    const path = isNext
+        ? 'M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z'
+        : 'M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z'
+    return (
+        <svg
+            className="size-5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+            data-slot="icon">
+            <path fillRule="evenodd" d={path} clipRule="evenodd" />
+        </svg>
+    )
+}
+
+const PaginationLinks = ({ index, totalPages, link }) => {
+    if (index === 0 || index === totalPages) {
+        return (
+            <a
+                href={link}
+                className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700">
+                <span className="sr-only">
+                    {index === 0 ? 'Previous' : 'Next'}
+                </span>
+                <PaginationLogo isNext={index === 0} />
+            </a>
+        )
+    } else {
+        return (
+            <a
+                href={link}
+                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
+                {index}
+            </a>
+        )
+    }
+}
+
+export const TablePagination = ({ rows }) => {
     return (
         <nav
             className="pt-4 flex justify-center"
             aria-label="Page navigation example">
             <ul className="flex items-center -space-x-px h-8 text-sm">
-                <li>
-                    <a
-                        href="#"
-                        className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700">
-                        <span className="sr-only">Previous</span>
-                        <svg
-                            className="w-2.5 h-2.5 rtl:rotate-180"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 6 10">
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M5 1 1 5l4 4"
+                {rows.map((row, index) => {
+                    return (
+                        <li key={index}>
+                            <PaginationLinks
+                                index={index}
+                                totalPages={rows.length}
+                                link={row}
                             />
-                        </svg>
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#"
-                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
-                        1
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#"
-                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
-                        2
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#"
-                        aria-current="page"
-                        className="z-10 flex items-center justify-center px-3 h-8 leading-tight text-blue-600 border border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700">
-                        3
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#"
-                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
-                        4
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#"
-                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
-                        5
-                    </a>
-                </li>
-                <li>
-                    <a
-                        href="#"
-                        className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700">
-                        <span className="sr-only">Next</span>
-                        <svg
-                            className="w-2.5 h-2.5 rtl:rotate-180"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 6 10">
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="m1 9 4-4-4-4"
-                            />
-                        </svg>
-                    </a>
-                </li>
+                        </li>
+                    )
+                })}
             </ul>
         </nav>
     )
 }
-
-export default TablePagination
