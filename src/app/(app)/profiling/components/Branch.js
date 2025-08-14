d9a4cf40f612a723949f345ab2573d5e7e1eadd1
JLemLip <johnlemuellipio@gmail.com>
2025-08-14 13:35:07 +0800

    added initial profiling page

---
+++ src/app/(app)/profiling/components/Branch.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+const Branch = () => {
+    return (
+        <div className="">
+            <h1>Branch Profiling</h1>
+            <p>table</p>
+        </div>
+    )
+}
+
+export default Branch
---
+++ src/app/(app)/profiling/components/Department.js
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+
+const Department = () => {
+    return (
+        <div className="">
+            <h1>Department Profiling</h1>
+            <p>table</p>
+        </div>
+    )
+}
+
+export default Department
